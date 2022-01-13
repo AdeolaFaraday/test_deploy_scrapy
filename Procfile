f6e91e7abed65c544d151f28493cbd75ca831d8b
adeolapunch <apanisile@punch.cool>
2022-01-13 08:09:44 -0800

    updates

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: scrapyd
+web: scrapyrt -i 0.0.0.0 -p $PORT
--- test_deploy/spiders/testdeploy.py
+++ test_deploy/spiders/testdeploy.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import os
 import scrapy
 from scrapy.selector import Selector
 from selenium import webdriver
@@ -13,11 +14,12 @@ class TestdeploySpider(scrapy.Spider):
     start_urls = ['http://www.fzmovies.net/']
 
     def parse(self, response):
-        chrome_options = Options()
-        chrome_options.add_argument('--headless')
-        chrome_path = which("chromedriver")
-        driver = webdriver.Chrome(
-            executable_path=chrome_path, options=chrome_options)
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
+        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
         driver.set_window_size(1920, 1080)
         driver.get("https://www.fzmovies.net/")
 
